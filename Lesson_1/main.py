f3494b77b23e3f955023dd742401627ef60fb563
blanc <muamarmaga@gmail.com>
2023-06-16 22:30:25 +0700

    файл вебинара

--- Lesson_1/main.py
+++ Lesson_1/main.py
@@ -1,1 +1,10 @@
-print(3+2)
+def strcount(s): # решение за О()
+    mydct = {}
+    for sym in s:
+        mydct[sym] = mydct.get(sym, 0) + 1
+
+    for key in mydct:
+        print(key, mydct.get(key))
+
+
+strcount('aabcde')
